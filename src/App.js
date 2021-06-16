72fb081ee8df045ee6b7db76f039c7c3c7395b92
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-06-16 10:54:44 -0400

    Built folder structure.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from "./pages/Homepage";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
