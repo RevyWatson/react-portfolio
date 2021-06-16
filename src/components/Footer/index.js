040227ae854640222b5dd1e35eaaaf0e45d31db3
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-06-16 18:51:23 -0400

    Updated css files.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,12 @@ import "./style.css";
 
 function Footer() {
     return (
-        <div class="container-fluid text-center">
-            <footer class="p-3">
-                Created with <i class="far fa-heart"></i> by Alissa Garzoni
-            </footer>
+        <div class="container-fluid text-center p-5">
+            <div className="row">
+                <footer class="p-4 col-12">
+                    Created with <i class="far fa-heart"></i> by Alissa Garzoni
+                </footer>
+            </div>
         </div>
     )
 }
