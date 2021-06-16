c3173d30ce29bdcc4ce4d1af0e369d76a79b4ba3
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-06-16 15:50:24 -0400

    Updated App.js.

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import Contact from "./pages/Contact";
 function App() {
   return (
     <Router>
+      <Header />
       <div>
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/gallery" component={Gallery} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/gallery" component={Gallery} />
+        <Route exact path="/contact" component={Contact} />
       </div>
+      <Footer />
     </Router>
   );
 }
