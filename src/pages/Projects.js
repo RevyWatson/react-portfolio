d5ce74aa937ad3bdf5d2c7c2e0d7b20151f5b657
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-06-18 22:24:39 -0400

    Rearranged folders and files and links - images all work now!

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
                 </div>
                 <Project
                     name="The Bashful Diner"
-                    image="./images/bashfuldiner.png"
+                    image="../images/bashfuldiner.png"
                     link="http://www.thebashfuldiner.com/"
                     description="The Bashful Diner is an application that facilitates a simple and streamlined dining experience for both guests and restaurant staff."
                     github="https://github.com/RevyWatson/the-bashful-diner"
@@ -20,7 +20,7 @@ function Projects() {
 
                 <Project
                     name="Note Taker"
-                    image="./images/notetaker.png"
+                    image="../images/notetaker.png"
                     link="https://ancient-atoll-40268.herokuapp.com/"
                     description="Note Taker is an application that can be used to write and save notes."
                     github="https://github.com/RevyWatson/note-taker"
@@ -28,7 +28,7 @@ function Projects() {
 
                 <Project
                     name="Tech Blog"
-                    image="./images/techblog.png"
+                    image="../images/techblog.png"
                     link="https://tranquil-shore-17488.herokuapp.com/"
                     description="CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts as well."
                     github="https://github.com/RevyWatson/tech-blog"
@@ -36,7 +36,7 @@ function Projects() {
 
                 <Project
                     name="Wanderlost"
-                    image="./images/wanderlost.jpg"
+                    image="../images/wanderlost.png"
                     link="https://dracon75.github.io/Project-1/"
                     description="Michigan hiking trail locator that provides basic trail
 info and current weather."
@@ -45,7 +45,7 @@ info and current weather."
 
                 <Project
                     name="Work Day Scheduler"
-                    image="./images/workdayscheduler.png"
+                    image="../images/workdayscheduler.png"
                     link="https://revywatson.github.io/work-day-scheduler/"
                     description="Event planning application to help organize a user's work schedule for the current day. Linked to current local date and time."
                     github="https://github.com/RevyWatson/work-day-scheduler"
@@ -53,7 +53,7 @@ info and current weather."
 
                 <Project
                     name="Password Generator"
-                    image="./images/passwordgenerator.png"
+                    image="../images/passwordgenerator.png"
                     link="https://revywatson.github.io/password_generator/"
                     description="Designed to generate a password that allows the user to specify
 parameters such as length and types of characters."
